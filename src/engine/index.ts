9557ebf62509afa68e8d07656310b1ede6a11f29
kevin <szy_dream@outlook.com>
2026-06-04 00:23:18 +0800

    feat(engine): add unified engine exports

---
+++ src/engine/index.ts
@@ -0,0 +1,7 @@
+// src/engine/index.ts
+export { PluginRegistry } from './PluginRegistry';
+export { MessageRouter } from './MessageRouter';
+export { LifecycleManager } from './LifecycleManager';
+export { DEFAULT_MANIFEST } from './PluginManifest';
+export type { PluginManifestEntry } from './PluginManifest';
+export type { PluginEntry } from './types';
